e4c0a0b60fef465f41b36ebc1533c985c25e0a0a
julienlaridant <70692611+julienlaridant@users.noreply.github.com>
2021-03-25 17:05:32 +0100

    Add files via upload

--- exercice-c à rendre/exercice remediation/exercice1.c
+++ exercice-c à rendre/exercice remediation/exercice1.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 int main()
 {
+    char caractere[6];
+    printf("entrez un nombre");
+   scanf("%d", caractere); 
+    if(strlen(caractere)>6)
+        {
+            printf("nombre trop grand");
+        }
+    
+
+    printf("%s\n", caractere);
+    return 0;
 
 }
